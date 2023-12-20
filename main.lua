d5562833976ee1206948cffaa9862cdd96644a03
Near <140867266+goinglikeatrainlol@users.noreply.github.com>
2023-12-21 00:14:40 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,52 +1,45 @@
 -- xspy > nano, dont get rune/vision
 -- nano is a retard xspy is smarter :P
-
 -- xspy on top, https://discord.gg/zgGJJCqfwx
-local key = Enum.KeyCode.Q -- litteraly not used
-part = "Head"
-activator = false
-locker = false
-game.UserInputService.InputBegan:Connect(
-    function(input)
-        if input.KeyCode == Enum.KeyCode.Q then
-            if activator then
-                activator = false
-            else
-                activator = true
-            end
+
+-- Near (Raven B4) was here :3
+
+local part = "Head"
+local activator = false
+local AimbotMouseDown = false
+local player = nil
+local lowest
+local LocalPlayer = game.Players.LocalPlayer
+local UIS = game.UserInputService
+
+UIS.InputBegan:Connect(function(input)
+    if input.KeyCode == Enum.KeyCode.Q then
+        activator = not activator
+    end
+    if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+        AimbotMouseDown = true
+    end
+end)
+UIS.InputEnded:Connect(function(input)
+    if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+        AimbotMouseDown = false
+    end
+end)
+local function whereclosetplayer() --done a function since it looks better + it can be used for other modules with this
+    lowest = math.huge
+    for i, v in pairs(game.Players:GetChildren()) do
+        local mag = (LocalPlayer.Character.HumanoidRootPart.Position - v.Character.HumanoidRootPart.Position).magnitude
+        if mag < lowest and v ~= LocalPlayer then
+            lowest = mag
+            player = v
         end
-    end
-)
-game.Players.LocalPlayer:GetMouse().Button2Down:Connect(
-    function()
-        locker = true
-    end
-)
-game.Players.LocalPlayer:GetMouse().Button2Up:Connect(
-    function()
-        locker = false
-    end
-)
-while wait() do
-    if locker and activator then
-        lowest = math.huge
-        for i, v in pairs(game.Players:GetChildren()) do
-            if
-                (game.Players.LocalPlayer.Character.HumanoidRootPart.Position -
-                    v.Character.HumanoidRootPart.Position).magnitude < lowest and
-                    v ~= game.Players.LocalPlayer
-             then
-                lowest =
-                    (game.Players.LocalPlayer.Character.HumanoidRootPart.Position -
-                    v.Character.HumanoidRootPart.Position).magnitude
-                player = v
-            end
-        end
-        workspace.CurrentCamera.CFrame =
-            CFrame.new(workspace.CurrentCamera.CFrame.Position, player.Character[part].Position)
     end
 end
--- xspy on top, https://discord.gg/zgGJJCqfwx
-
--- xspy > nano, dont get rune/vision
--- nano is a retard xspy is smarter :P
+while wait() do -- repeat would be much cleaner since it can be stopped but alr
+    if AimbotMouseDown and activator then
+        player = whereclosetplayer()
+        if player ~= nil then
+            workspace.CurrentCamera.CFrame = CFrame.new(workspace.CurrentCamera.CFrame.Position, player.Character[part].Position)
+        end
+    end
+end
